34b9ea26330febb92ec7c9fdbfe7bd0744d6170f
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-09-05 09:41:02 +0000

    izmaiņas 2.0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,24 @@
 package lv.rvt;
 
+
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ievadi savu vardu:");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Sveiks " + name);
+
         System.out.println("Hello RVT!");
+        int age = 17;
+        boolean isOnline = true;
+        
     }
 
 }
