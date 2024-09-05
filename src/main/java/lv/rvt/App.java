fde04de645dfd2546f6a9a8478bd971faac742ca
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-09-05 10:27:38 +0000

    Various Variables

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,24 +1,15 @@
 package lv.rvt;
 
 
-import java.util.Scanner;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+        int chicken = 3;
 
-        System.out.println("Ievadiet savu vardu:");
-        String name = scanner.nextLine();
 
-        System.out.println("Ievadiet savu uzvardu:");
-        String username = scanner.nextLine();
-
-        System.out.println("Ievadiet savu gruppu:");
-        String gruppa = scanner.nextLine();
-
-        System.out.println(name + " " + username + " " + gruppa + " " );
         
         
     }
