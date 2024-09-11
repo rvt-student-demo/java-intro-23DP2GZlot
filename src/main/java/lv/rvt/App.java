dab798491538896f0bd9133f6e9c213dd1e5cbe0
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-09-11 10:40:05 +0000

    Uzdevums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,27 +1,34 @@
 package lv.rvt;
 
 
-
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        int chicken = 3;
-        double bacon = 5.5;
-        String tractor = "None!";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Chicken:");
-        System.out.println(3);
-        System.out.println("Bacon(kg):");
-        System.out.println(5.5);
-        System.out.println("Tractor:");
-        System.out.println(tractor);
-        System.out.println("  ");
-        System.out.println("And finally, a summary:");
-        System.out.println(3);
-        System.out.println(5.5);
-        System.out.println(tractor);
+        String userInput = scanner.nextLine();
+
+        System.out.println("Ievadi savu vārdu: ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvardu: ");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Ievadi savu gruppu: ");
+        String group = scanner.nextLine();
+        
+        System.out.println("Students: " + name + " " + lastname + ", " + "grupa: " + group);
+
+
+
+        
+
+
+
         
 
 
