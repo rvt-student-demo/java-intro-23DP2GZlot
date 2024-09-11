455b42c0a568c093ab6db171e7fe36c2c10b3cb4
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-09-11 10:11:31 +0000

    Various Variables 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,6 +8,24 @@ public class App
     public static void main( String[] args )
     {
         int chicken = 3;
+        double bacon = 5.5;
+        String tractor = "None!";
+
+        System.out.println("Chicken:");
+        System.out.println(3);
+        System.out.println("Bacon(kg):");
+        System.out.println(5.5);
+        System.out.println("Tractor:");
+        System.out.println(tractor);
+        System.out.println("  ");
+        System.out.println("And finally, a summary:");
+        System.out.println(3);
+        System.out.println(5.5);
+        System.out.println(tractor);
+        
+
+
+
 
 
         
