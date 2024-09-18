c5c646d60981d8429c8b15d0db4c5aa51db205d8
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-09-18 10:57:39 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,30 +6,18 @@ public class App
 {
 
     public static void main ( String[] args ) {
+        
         Scanner reader = new Scanner(System.in);
-        System.out.println("Give points [0-100]:");
-        int number = Integer.valueOf(reader.nextLine());  
+        int number = Integer.valueOf(reader.nextLine());
         
-
-        if (number < 0) {
-            System.out.println(" Grade: impossible!");
-        } else if (number <= 49) {
-            System.out.println("Grade: failed");
-        } else if (number <= 59) {
-            System.out.println("Grade: 1");
-        } else if (number <= 69) {
-            System.out.println("Grade: 2");
-        } else if (number <= 79) {
-            System.out.println("Grade: 3");
-        } else if (number <= 89) {
-            System.out.println("Grade: 4");
-        } else if (number <= 100) {
-            System.out.println("Grade: 5");
-        } else if (number > 100) {
-            System.out.println("Grade: incredible!");
-        
-        
-            
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
         }
     }
 
