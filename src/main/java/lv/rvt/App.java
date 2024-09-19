fa6129276d8d7cd2ac7cf4a31397f5156c05b181
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-09-19 09:37:02 +0000

    Comparing Numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,32 +7,19 @@ public class App
     
 
     public static void main ( String[] args ) {
-        
         Scanner reader = new Scanner(System.in);
-        System.out.println("Value of the gift?");
-        int Itemprice = Integer.valueOf(reader.nextLine());
-
-        if (Itemprice >= 5000 && Itemprice < 25000) {
-            System.out.println("Tax: " + (100+ (Itemprice - 5000) * 0.08));
-
-        } else if (Itemprice < 5000) {
-            System.out.println("No tax!");
-
-        } else if (Itemprice >= 25000 && Itemprice < 55000) {
-            System.out.println("Tax: " + (1700 + (Itemprice - 25000) * 0.10));
-
-        } else if (Itemprice >= 55000 && Itemprice < 200000) {
-            System.out.println("Tax: " + (4700 + (Itemprice - 55000) * 0.12));
-
-        } else if (Itemprice >= 200000 && Itemprice < 1000000) {
-            System.out.println("Tax: " + (22100 + (Itemprice - 200000) * 0.15));
-
-        } else if (Itemprice >= 1000000) {
-            System.out.println("Tax: " + (142100 + (Itemprice - 1000000) * 0.17));
        
-    }
+        int first = Integer.valueOf(reader.nextLine());
+        int second = Integer.valueOf(reader.nextLine());
 
-
+        if (first > second) {
+            System.out.println(first + " is greater than " + second);
+        }if (first < second) {
+            System.out.println(first + " is smaller than " + second);
+        }if (first == second) {
+            System.out.println(first + " is equal than " + second);
+            
+}
 }
 }
 
