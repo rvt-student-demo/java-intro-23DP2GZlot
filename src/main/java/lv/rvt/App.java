932e9fe7c13e0ff006375de3f78709032db0d5bc
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-09-19 08:34:57 +0000

    Gift Tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,24 +4,36 @@ import java.util.Scanner;
 
 public class App 
 {
+    
 
     public static void main ( String[] args ) {
         
         Scanner reader = new Scanner(System.in);
-        int number = Integer.valueOf(reader.nextLine());
-        
-        if (number % 3 == 0 && number % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (number % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (number % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(number);
-        }
+        System.out.println("Value of the gift?");
+        int Itemprice = Integer.valueOf(reader.nextLine());
+
+        if (Itemprice >= 5000 && Itemprice < 25000) {
+            System.out.println("Tax: " + (100+ (Itemprice - 5000) * 0.08));
+
+        } else if (Itemprice < 5000) {
+            System.out.println("No tax!");
+
+        } else if (Itemprice >= 25000 && Itemprice < 55000) {
+            System.out.println("Tax: " + (1700 + (Itemprice - 25000) * 0.10));
+
+        } else if (Itemprice >= 55000 && Itemprice < 200000) {
+            System.out.println("Tax: " + (4700 + (Itemprice - 55000) * 0.12));
+
+        } else if (Itemprice >= 200000 && Itemprice < 1000000) {
+            System.out.println("Tax: " + (22100 + (Itemprice - 200000) * 0.15));
+
+        } else if (Itemprice >= 1000000) {
+            System.out.println("Tax: " + (142100 + (Itemprice - 1000000) * 0.17));
+       
     }
 
 
+}
 }
 
 
