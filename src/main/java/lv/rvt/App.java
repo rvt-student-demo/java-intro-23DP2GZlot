f7fda1b4093bfe724008574bdecc0e00fbcd08a1
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-10-03 08:25:08 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,49 +4,24 @@ import java.util.Scanner;
 
 public class App 
 {
-    
-
-    public static void main ( String[] args ) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number1 = 2;
+        int number2 = 10;
+        divisibleByThreeInRange(2,10);
         
-        int sum = 0;
-        int ones = 0;
-        int evenNumbers = 0;
-        int oddNumbers = 0;
-        System.out.println("Give a number:");
-            while (true) {
-                int number = Integer.valueOf(scanner.nextLine());
+    
+    }
+    public static void divisibleByThreeInRange(int beginning, int end){
+        while (beginning <= end){
+            if (beginning % 3 == 0){
+                System.out.println(beginning);
                 
-                 if (number == -1) {
-                    System.out.println("Thx! Bye!");
-                    System.out.println("Sum: " + sum );
-                    System.out.println("Numbers: " + ones );
-                    System.out.println("Average: " + (double)sum / ones );
-                    System.out.println("Even: " + evenNumbers);
-                    System.out.println("Odd: " + oddNumbers);
-                    break;
-                 }
-                    if (number % 2 == 0){
-                        evenNumbers++;
-
-                    if (number % 2 == 0){
-                        evenNumbers++;
-                    }
-
-
-                       
-                 }
-                else{
-                    ones = ones + 1;
-                    sum = sum + number;
-
-
-                    continue;
-                 }
-                 
+            }
+            beginning++;
+        }
+    }
 }
-    }
-    }
         
         
 
