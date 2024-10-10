f6ee67f7fa016358b9db51726b05f59c3bd6983a
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-10-10 08:57:25 +0000

    Star sign (4 parts)

---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,56 @@
+package lv.rvt;
+
+import java.util.*;
+
+public class Stars {
+
+    public static void main (String[] args) {
+
+        printStars(5);
+        
+        System.out.println("");
+        
+        printStars(3);
+        
+        System.out.println("");
+    
+        printStars(9);
+        System.out.println("");
+    
+    
+    }
+    
+        public static void printStars(int number) {
+            for (int counter = 0; counter < number; counter++){
+            
+            System.out.print("*");
+            
+            }
+        }
+    }
+
+    
+    
+
+
+
+
+    
+
+
+
+
+          
+
+
+        
+        
+
+
+
+
+
+
+
+
+
