76f74975b3f0aabeb2c1b5229cf6acd904134da8
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-10-17 09:56:36 +0000

    Exercise 2 — Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,14 +5,40 @@ import java.util.*;
 public class App {
     
         public static void main(String[] args) {
-            Stars.printTriangle(4);
+            Scanner scanner = new Scanner(System.in);
+ArrayList<Integer> numbers = new ArrayList<>();
+while (true) {
+
+int number = Integer.valueOf(scanner.nextLine());
+
+if (number == 0) {
+
+break;
+
+}
+
+numbers.add(number);
+}
+int total_sum = 0;
+for(int sum : numbers){
+total_sum += sum;
+}
+
+
+System.out.println(numbers.get(1)+numbers.get(2));
+System.out.println(total_sum);
+        }
+}
+
+            
+
             
             
             
             
         
-    }
-}
+    
+
 
     
     
