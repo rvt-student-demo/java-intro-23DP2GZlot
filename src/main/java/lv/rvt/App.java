d86d22a98259ffd1222b0e027d9040ab9973bfbb
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-10-30 11:11:51 +0000

    Product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,34 +1,41 @@
 package lv.rvt;
 
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class App {
-    
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-ArrayList<Integer> numbers = new ArrayList<>();
-while (true) {
-
-int number = Integer.valueOf(scanner.nextLine());
-
-if (number == 0) {
-
-break;
-
-}
-
-numbers.add(number);
-}
-int total_sum = 0;
-for(int sum : numbers){
-total_sum += sum;
-}
-
-
-System.out.println(numbers.get(1)+numbers.get(2));
-System.out.println(total_sum);
+        public static void main(String[] srgs){
+                Scanner scanner = new Scanner(System.in);
+                int secretNumber = 1 + (int)(10 * Math.random());  
+                int maxAttempts = 3;  
+                int attempts, guess;  
+                System.out.println("I am thinking of a number from 1 to 10.");  
+                System.out.println("You must guess what it is in three tries.");   
+                System.out.println( "Enter your guess:"); 
+                for (attempts = 0; attempts < maxAttempts; attempts++) {    
+                    guess = scanner.nextInt();  
+                    if (secretNumber == guess) {  
+                        System.out.println("RIGHT!\r\n" + //
+                                         "You have won the game.");  
+                        break;  
+                    } else if (secretNumber > guess && attempts != maxAttempts - 1) {  
+                        System.out.println("wrong");  
+                    } else if (secretNumber < guess && attempts != maxAttempts - 1) {  
+                        System.out.println("wrong");  
+                    }  
+                }  
+                if (attempts == maxAttempts) {  
+                    System.out.println("You have lost the game.");
+                    System.out.println("The correct number was " + secretNumber);  
+                }  
+            }  
         }
-}
+
+
+        
+    
+        
+
 
             
 
