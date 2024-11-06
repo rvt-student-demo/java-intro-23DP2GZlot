99f33f55d2374ee3fa78c31434df3cecc638016b
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-11-06 11:29:36 +0000

    Agent

--- src/main/java/lv/rvt/main.java
+++ src/main/java/lv/rvt/main.java
@@ -6,13 +6,12 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-      Product initialName = new Product("Banana",1.1,13);
-      initialName.printProduct();
       
+  }
 
         
     }
-}
+
 
 
         
