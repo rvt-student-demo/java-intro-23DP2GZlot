8ab238b8e2219b7a41c17cbeb502691e33e72690
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-11-07 09:09:22 +0000

    Payment Card (6 parts)

--- src/main/java/lv/rvt/main.java
+++ src/main/java/lv/rvt/main.java
@@ -1,54 +1,40 @@
 package lv.rvt;
 
-import java.util.Scanner;
+import java.util.*;
 
 
 
 public class main {
-    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+      PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
 
-        Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
-        System.out.println("Sum: " + statistics.sum());
-        System.out.println("Average: " + statistics.average());
-    
-        
-        System.out.println("Enter numbers:");
 
-        Statistics statistics1 = new Statistics();
-        Statistics even = new Statistics();
-        Statistics odd = new Statistics();
 
-        while (true) {
 
-            int input = Integer.valueOf(scanner.nextLine());
 
-            if (input == -1) {
-                break;
-            }
 
-            statistics.addNumber(input);
-            
-            if (input % 2 == 0) {
-                even.addNumber(input);
-            }
-            else {
-                odd.addNumber(input);
-            }
-            
-        }
 
-        System.out.println("Sum: " + statistics.sum());
-        System.out.println("Sum of even numbers: " + even.sum());
-        System.out.println("Sum of odd numbers: " + odd.sum());
+
+
     }
-      
   }
+      
+      
+  
 
         
     
