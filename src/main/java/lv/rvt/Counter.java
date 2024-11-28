76c75b52ea6b34ccc1b75c3c48cddd9a71790e36
23DP2GZlot <166400412+23DP2GZlot@users.noreply.github.com>
2024-11-28 10:28:49 +0000

    Count

--- src/main/java/lv/rvt/Counter.java
+++ src/main/java/lv/rvt/Counter.java
@@ -1,48 +1,43 @@
 package lv.rvt;
 
 public class Counter {
-    private int value;   // a variable that remembers the value of the counter
+    private int counter;
 
-    public Counter(int initialValue) {
-        this.value = initialValue;
-        private int counter;
+    public Counter(int startValue) {
+        this.counter = startValue;
+    }
 
-        public Counter(int startValue) {
-            this.counter = startValue;
+    public Counter() {
+        this(0);
+    }
+
+    public int value() {
+        return this.counter;
+    }
+
+    public void increase() {
+        this.counter++;
+    }
+
+    public void decrease() {
+        this.counter--;
+    }
+
+    public void increase(int increaseBy) {
+        if (increaseBy >= 0) {
+            this.counter = this.counter + increaseBy;
         }
+    }
+
+    public void decrease(int decreaseBy) {
+        if (decreaseBy >= 0) {
+            this.counter = this.counter - decreaseBy;
+        }
+    }
     
-        public Counter() {
-            this(0);
-        }
-    
-        public int value() {
-            return this.counter;
-        }
-    
-        public void increase() {
-            this.counter++;
-        }
-    
-        public void decrease() {
-            this.counter--;
-        }
-    
-        public void increase(int increaseBy) {
-            if (increaseBy >= 0) {
-                this.counter = this.counter + increaseBy;
-            }
-        }
-    
-        public void decrease(int decreaseBy) {
-            if (decreaseBy >= 0) {
-                this.counter = this.counter - decreaseBy;
-            }
-        }
-        
-        @Override
-        public String toString(){
-            return "Count: " + this.counter;
-        }
-    
+    @Override
+    public String toString(){
+        return "Count: " + this.counter;
+    }
 }
-}
+
